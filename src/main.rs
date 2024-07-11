369457ec5f36632568d60ef90c2c797ad4109beb
Mike Dilger <mike@mikedilger.com>
2024-07-11 17:16:07 +1200

    Fix usage claiming to be a different program

--- src/main.rs
+++ src/main.rs
@@ -25,56 +25,56 @@ fn main() -> Result<(), Error> {
     let _ = args.next().expect("Command was not given arg[0]");
     let url_text = args
         .next()
-        .expect("Usage: relay-tester <url> <command> [args] ...");
+        .expect("Usage: relay-manager <url> <command> [args] ...");
     let uri = Uri::from_str(&url_text)?;
     let command = args
         .next()
-        .expect("Usage: relay-tester <url> <command> [args] ...");
+        .expect("Usage: relay-manager <url> <command> [args] ...");
     match &*command {
         "supportedmethods" | "listbannedpubkeys" | "listallowedpubkeys" | "listeventsneedingmoderation" |
         "listbannedevents" | "listallowedkinds" | "listblockedips" => post(uri, &*command, json!([]))?,
         "banpubkey" => {
-            let pubkeytext = args.next().expect("Usage: relay-tester banpubkey <pubkey>");
+            let pubkeytext = args.next().expect("Usage: relay-manager banpubkey <pubkey>");
             post(uri, &*command, json!([pubkeytext]))?
         },
         "allowpubkey" => {
-            let pubkeytext = args.next().expect("Usage: relay-tester allowpubkey <pubkey>");
+            let pubkeytext = args.next().expect("Usage: relay-manager allowpubkey <pubkey>");
             post(uri, &*command, json!([pubkeytext]))?
         },
         "allowevent" => {
-            let idtext = args.next().expect("Usage: relay-tester allowevent <id>");
+            let idtext = args.next().expect("Usage: relay-manager allowevent <id>");
             post(uri, &*command, json!([idtext]))?
         },
         "banevent" => {
-            let idtext = args.next().expect("Usage: relay-tester banevent <id>");
+            let idtext = args.next().expect("Usage: relay-manager banevent <id>");
             post(uri, &*command, json!([idtext]))?
         },
         "changerelayname" => {
-            let newname = args.next().expect("Usage: relay-tester changerelayname <newname>");
+            let newname = args.next().expect("Usage: relay-manager changerelayname <newname>");
             post(uri, &*command, json!([newname]))?
         },
         "changerelaydescription" => {
-            let newdesc = args.next().expect("Usage: relay-tester changerelaydescription <newname>");
+            let newdesc = args.next().expect("Usage: relay-manager changerelaydescription <newname>");
             post(uri, &*command, json!([newdesc]))?
         },
         "changerelayicon" => panic!("changerelayicon is unimplemented"),
         "allowkind" => {
-            let kind = args.next().expect("Usage: relay-tester allowkind <kind>");
+            let kind = args.next().expect("Usage: relay-manager allowkind <kind>");
             post(uri, &*command, json!([kind]))?
         },
         "disallowkind" => {
-            let kind = args.next().expect("Usage: relay-tester disallowkind <kind>");
+            let kind = args.next().expect("Usage: relay-manager disallowkind <kind>");
             post(uri, &*command, json!([kind]))?
         },
         "blockip" => {
-            let ip = args.next().expect("Usage: relay-tester blockip <ip>");
+            let ip = args.next().expect("Usage: relay-manager blockip <ip>");
             post(uri, &*command, json!([ip]))?
         },
         "unblockip" => {
-            let ip = args.next().expect("Usage: relay-tester unblockip <ip>");
+            let ip = args.next().expect("Usage: relay-manager unblockip <ip>");
             post(uri, &*command, json!([ip]))?
         },
-        _ => panic!("Usage relay-tester <command> [args]...\n
+        _ => panic!("Usage relay-manager <command> [args]...\n
 Available Commands:  supportedmethods, banpubkey, listbannedpubkeys, allowpubkey, listallowedpubkeys, listeventsneedingmoderation, allowevent, banevent, listbannedevents, changerelayname, changerelaydescription, changerelayicon, allowkind, disallowkind, listallowedkinds, blockip, unblockip, listblockedips"),
     }
 
